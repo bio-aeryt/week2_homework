bfccda6bcbfd3095a281e63fe74e450c4a5881b6
bio-aeryt <krhribuki0417@gmail.com>
2024-12-12 08:37:18 +0900

    ver_1

---
+++ HackersNews.py
@@ -0,0 +1,34 @@
+import requests
+import time
+
+
+# トップページに表示されているニュースのタイトルおよびリンクURLを取得
+# 変数にHTTPライブラリで、APIの情報を取得して代入する
+
+top_stories = "https://hacker-news.firebaseio.com/v0/topstories.json"
+
+response = requests.get(top_stories)
+
+# jsonデータを取得し、最初の5件のIDを取得
+story_ids = response.json()[:5]
+
+# IDから各ニュースの詳細を表示 かつ、負荷かけないようにするために、time関数を導入（先ほどはこれを入れず失敗）
+# enumerate関数を用いて、idと、index番号を同時に取得
+
+def main(story_ids):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    for story_id in story_ids:
+        time.sleep(1)  
+        story_info = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json", headers=headers)
+
+        if story_info.status_code == 200:
+            dic = story_info.json()
+            dic_output = {f"title: {dic['title']}, link: {dic['url']}"}
+            print(dic_output)
+
+        else:
+            print(story_info.status_code)
+
+
+if __name__ == "__main__":
+    main(story_ids)
